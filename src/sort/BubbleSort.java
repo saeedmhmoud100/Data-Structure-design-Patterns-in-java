d03d2b39d5dd46f4deb79becbaf36639ec2d8fae
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-03-09 16:23:50 +0200

    add some change in the bubble sort function to make best case is better

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -4,14 +4,19 @@ public class BubbleSort {
     public static void BubbleSort(int arr[]){
         double c = System.currentTimeMillis();
         for(int i=0;i<arr.length;i++){
+            boolean swap = false;  //------ to check if its sorted then break
             for(int j = 0; j < arr.length-i-1;j++){
 
                 if(arr[j] > arr[j+1]){
+                    swap=true; //// -----
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
 
+            }
+            if(!swap){ //// ------
+                break;
             }
         }
         c= System.currentTimeMillis() - c;
